75d380ee7fa497e70645edcc320e352b7f7cb35a
chandan-16 <chandangautam420.cg@gmail.com>
2026-07-28 17:35:39 +0530

    Created seperate component for the login page to make the reusable component

---
+++ src/components/Login.tsx
@@ -0,0 +1,16 @@
+import { Input } from "./Input"
+
+export const Login = () => {
+  return (
+    <>
+        <div className="w-full md:w-[450px]">
+            <h1 className='text-white text-center font-bold text-4xl md:text-6xl mb-10'>Login</h1>
+            <div className="bg-white flex flex-col w-full gap-3 p-6 min-h-[150px] rounder-xl rounded-xl drop-shadow-xl">
+                <Input name="name" type="email" />
+                <Input name="password" type="password"/>
+                <Input name="confirm-password" type="password"/>
+            </div>
+        </div>    
+    </>
+  )
+}
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,9 @@
-import React from 'react'
-import { Input } from '../components/Input'
+import { Login } from '../components/Login'
 
 export const LoginPage = () => {
   return (
     <div className='bg-blue-600 h-[100vh] flex items-center justify-center p-10 '>
-        <div className="w-full  md:w-[450px]">
-            <h1 className='text-white text-center font-bold text-4xl md:text-6xl mb-10'>Login</h1>
-            <div className="bg-white flex flex-col w-full gap-3 p-6 min-h-[150px] rounder-xl rounded-xl drop-shadow-xl">
-                 <Input name="name" type="email" />
-                 <Input name="password" type="password"/>
-            </div>
-        </div>
+      <Login />
     </div>
   )
 }
